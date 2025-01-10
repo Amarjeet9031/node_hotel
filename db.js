e08303704deff91cf026675e5377ffd602b609cc
Amarjeet9031 <amarjeet_223711@saitm.org>
2025-01-10 12:41:30 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,21 +1,23 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
+require('dotenv').config();
 
-const mongourl='mongodb://127.0.0.1:27017/hotels';
+// const mongourl = "mongodb://127.0.0.1:27017/hotels";
+const mongourl =
+  "mongodb+srv://Amarjeet_223711:Hostel90311@cluster1.2thf3.mongodb.net/";
 
-mongoose.connect(mongourl,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
-})
+mongoose.connect(mongourl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
 
-const db=mongoose.connection;
+db.on("error", (err) => {
+  console.log("disconnected to the mongodb server", err);
+});
 
-db.on('error', (err) => {
-  console.log('disconnected to the mongodb server',err);
-})
+db.on("connected", () => {
+  console.log("connected to the mongodb server");
+});
 
-db.on('connected', () => {
-  console.log('connected to the mongodb server');
-})
-
-module.exports=db;
+module.exports = db;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-
+const PORT=process.env.PORT||3000;
 
 
 app.get("/", function (req, res) {
@@ -19,6 +20,9 @@ app.use('/person',personRouters);
 
 const  menuRoutes=require('./rotues/menuRoutes');
 app.use('/menu',menuRoutes);
+
+
+
 app.listen(3000, () => {
   console.log("server run on that address 3000");
 });
